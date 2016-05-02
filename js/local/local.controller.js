2cc0a9408e184662cde2f49f7196ce9b1a606464
Steven Brice <brice.steven@gmail.com>
2016-05-02 01:00:17 -0500

    Group events by each venue

--- js/local/local.controller.js
+++ js/local/local.controller.js
@@ -8,8 +8,67 @@
     .module('app.local', [])
     .controller('LocalController', LocalController);
 
-  LocalController.$inject = ['$rootScope', '$scope', '$http', '$state'];
-  function LocalController($rootScope, $scope, $http, $state) {
-    //
+  LocalController.$inject = ['$rootScope', '$scope', '$http', '$state', 'VividSeatsService'];
+  function LocalController($rootScope, $scope, $http, $state, VSeats) {
+    /**
+     * Store Events that will be displayed in the UI
+     * @type {Array}
+     */
+    $scope.eventsPerCity = {};
+    $scope.totalEvents = 0;
+
+    /**
+     * Turn Date String into date object
+     * @param dateString
+     * @returns {number}
+     */
+    $scope.getDate = function(dateString) {
+      return Date.parse(dateString);
+    };
+
+    /**
+     * Load Events from API
+     */
+    function loadEvents() {
+      VSeats.eventService.all(
+        function(evts){
+          $scope.$apply(function() {
+            $scope.totalEvents = evts.length;
+            groupByCity(evts);
+          });
+        },
+        function(msg){
+          $scope.$apply(function() {
+            alert('Error: ' + msg);
+            $scope.eventsPerCity = {};
+          });
+        }
+      );
+    }
+
+    /**
+     * Group the events by each day
+     * @param evts
+     */
+    function groupByCity(evts) {
+      evts.forEach(function(evt) {
+        var city = evt.venue.city + evt.venue.state;
+
+        // Check to see if the date has already been setup
+        if(typeof $scope.eventsPerCity[city] !== typeof {}) {
+
+          // Set the date and create an empty array for the events
+          $scope.eventsPerCity[city] = {
+            city: evt.venue.city,
+            events: []
+          };
+        }
+
+        // Add the event to the specific day
+        $scope.eventsPerCity[city].events.push(evt);
+      });
+    }
+
+    loadEvents();
   }
 })();
