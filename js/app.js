ce63706e5abb2184aa99f6ddbae8c9ae080c1122
Steven Brice <brice.steven@gmail.com>
2016-05-02 02:24:47 -0500

    Order events by respected sorted results

--- js/app.js
+++ js/app.js
@@ -20,6 +20,25 @@
     })
     .run(function(editableOptions) {
       editableOptions.theme = 'bs3'; // bootstrap3 theme
+    })
+    .filter('toArray', function () {
+      // Convert Array to object to allow ng-repeat to sort the dates by order
+      //https://github.com/petebacondarwin/angular-toArrayFilter/blob/master/toArrayFilter.js
+      return function (obj, addKey) {
+        if (!angular.isObject(obj)) return obj;
+        if ( addKey === false ) {
+          return Object.keys(obj).map(function(key) {
+            return obj[key];
+          });
+        } else {
+          return Object.keys(obj).map(function (key) {
+            var value = obj[key];
+            return angular.isObject(value) ?
+              Object.defineProperty(value, '$key', { enumerable: false, value: key}) :
+            { $key: key, $value: value };
+          });
+        }
+      };
     });
 
 })();
