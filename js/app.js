5b2acccfaf5eb142dfd90ae7548787ceb78a38b9
Steven Brice <brice.steven@gmail.com>
2016-05-02 00:13:45 -0500

    Wrap Vivid Seats API in factory

--- js/app.js
+++ js/app.js
@@ -11,6 +11,10 @@
       'app.overview',
       'app.local',
       'app.upcoming'
-    ]);
+    ])
+    .factory("VividSeatsService", function ($window) {
+      // Create a service to wrap access to the Global library
+      return $window.VividSeats;
+    });
 
 })();
