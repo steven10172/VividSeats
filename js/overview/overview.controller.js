a9ffc6d64f085bd04091d13e7902c8f2ecf0ddc4
Steven Brice <brice.steven@gmail.com>
2016-05-02 00:30:01 -0500

    Add basic documentation

--- js/overview/overview.controller.js
+++ js/overview/overview.controller.js
@@ -10,24 +10,40 @@
 
   OverviewController.$inject = ['$rootScope', '$scope', '$http', '$state', 'VividSeatsService'];
   function OverviewController($rootScope, $scope, $http, $state, VSeats) {
+    /**
+     * Store Events that will be displayed in the UI
+     * @type {Array}
+     */
     $scope.events = [];
 
+    /**
+     * Turn Date String into date object
+     * @param dateString
+     * @returns {number}
+     */
     $scope.getDate = function(dateString) {
       return Date.parse(dateString);
     };
 
-    VSeats.eventService.all(
-      function(evts){
-        $scope.$apply(function() {
-          $scope.events = evts;
-        });
-      },
-      function(msg){
-        $scope.$apply(function() {
-          alert('Error: ' + msg);
-          $scope.events = [];
-        });
-      }
-    );
+    /**
+     * Load Events from API
+     */
+    function loadEvents() {
+      VSeats.eventService.all(
+        function(evts){
+          $scope.$apply(function() {
+            $scope.events = evts;
+          });
+        },
+        function(msg){
+          $scope.$apply(function() {
+            alert('Error: ' + msg);
+            $scope.events = [];
+          });
+        }
+      );
+    }
+
+    loadEvents();
   }
 })();
